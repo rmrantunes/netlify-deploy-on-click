6bfa9416175223d645cda347b3aed9757045c3d7
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-19 13:33:25 -0400

    refactor: change to useStaging

---
+++ src/hooks/useStaging.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function useStaging(stagingBranchName = "dev") {
+  const [isStaging, setIsStaging] = useState(false);
+  const [deployId, setDeployId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const isStaging = window
+        ? window.location.href.split("//")[1].startsWith(stagingBranchName)
+        : false;
+
+      setIsStaging(isStaging);
+
+      if (isStaging) setDeployId(window.location.host);
+    }
+  }, []);
+
+  return { isStaging, deployId };
+}

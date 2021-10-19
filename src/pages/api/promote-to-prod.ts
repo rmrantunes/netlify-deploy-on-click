b726646bfb4be6e06bb32cbf88ea26a971c5207b
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-19 13:53:17 -0400

    fix: add error handling to promote-to-prod

--- src/pages/api/promote-to-prod.ts
+++ src/pages/api/promote-to-prod.ts
@@ -2,21 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const promoteEndpoint = `https://api.netlify.com/api/v1/deploys/${req.query.deploy_id}/restore`;
+    try {
+      const promoteEndpoint = `https://api.netlify.com/api/v1/deploys/${req.query.deploy_id}/restore`;
 
-    await axios.post(promoteEndpoint, undefined, {
-      headers: {
-        Authorization: "Bearer " + process.env.NETLIFY_ACCESS_TOKEN,
-      },
-    });
+      await axios.post(promoteEndpoint, undefined, {
+        headers: {
+          Authorization: "Bearer " + process.env.NETLIFY_ACCESS_TOKEN,
+        },
+      });
+    } catch (error) {
+      return res.json({ error });
+    }
   }
 }

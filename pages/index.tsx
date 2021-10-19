6359ee53bbe893233667f64e462a4383abe7e8e8
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-19 10:23:55 -0400

    fix: correct env verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   useEffect(() => {
-    console.log(process.env.IS_STAGING);
+    console.log(process.env.NODE_ENV);
   }, []);
 
   return (

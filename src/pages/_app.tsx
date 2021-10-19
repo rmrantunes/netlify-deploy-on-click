fd77ad18fa75fc54c5f7fb835fb7fb0699323fec
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-19 11:12:17 -0400

    feat; add staging button to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import useIsStaging from "../hooks/useIsStaging";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isStaging = useIsStaging();
+
+  return (
+    <>
+      {isStaging && (
+        <button style={{ position: "fixed", right: "3rem", top: "3rem" }}>
+          Aprovar e publicar
+        </button>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;

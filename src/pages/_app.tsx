8bfe5d44833e93a1edfef8708916257e42bed362
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-19 11:17:33 -0400

    feat: test call promote to prod if staging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import useIsStaging from "../hooks/useIsStaging";
+import axios from "axios";
+
+async function promoteToProd() {
+  await axios.post("/api/promote-to-prod");
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const isStaging = useIsStaging();
@@ -8,7 +13,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {isStaging && (
-        <button style={{ position: "fixed", right: "3rem", top: "3rem" }}>
+        <button
+          style={{ position: "fixed", right: "3rem", top: "3rem" }}
+          onClick={promoteToProd}
+        >
           Aprovar e publicar
         </button>
       )}

1373fe560a0329596156195f96ec9812441a3c6f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-20 09:38:34 -0400

    feat: check NODE_ENV

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    console.log(process.env.NODE_ENV);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
